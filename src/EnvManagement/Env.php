9fac4839d8dddfd8e5886ee3c31368e88e88f936
petr-buchyn <petr.buchyn@finsight.com>
2021-01-26 05:58:45 +0200

    EnvVar class deleted, method moved to Env class

--- src/EnvManagement/Env.php
+++ src/EnvManagement/Env.php
@@ -2,6 +2,10 @@
 
 namespace Dealroadshow\Bundle\K8SBundle\EnvManagement;
 
+use Closure;
+use ReflectionObject;
+use RuntimeException;
+
 class Env
 {
     const DEV = 'dev';
@@ -12,4 +16,31 @@ class Env
 
     // Pseudo env used in some classes like EnvAwareContainerMaker
     const DEFAULT = 'default';
+
+    private static Closure|null $getEnvClosure = null;
+
+    public static function getVar(string $name): mixed
+    {
+        $closure = self::getClosure();
+
+        return $closure($name);
+    }
+
+    private static function getClosure(): Closure
+    {
+        if (null === self::$getEnvClosure) {
+            $container = DIContainerRegistry::get();
+            $class = new ReflectionObject($container);
+            if (!$class->hasMethod('getEnv')) {
+                throw new RuntimeException(
+                    sprintf('DI container class "%s" does not have method "getEnv()"', $class->getName())
+                );
+            }
+            $method = $class->getMethod('getEnv');
+            $method->setAccessible(true);
+            self::$getEnvClosure = $method->getClosure($container);
+        }
+
+        return self::$getEnvClosure;
+    }
 }
