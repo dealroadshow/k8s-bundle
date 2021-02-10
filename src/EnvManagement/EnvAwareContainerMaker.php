cb0c85bbc06af10d959892ac87478d389d08e5e2
petr-buchyn <petr.buchyn@finsight.com>
2021-02-10 15:49:18 +0200

    Sync bundle with latest changes in dealroadshow/k8s-framework

--- src/EnvManagement/EnvAwareContainerMaker.php
+++ src/EnvManagement/EnvAwareContainerMaker.php
@@ -8,7 +8,7 @@ use Dealroadshow\K8S\Framework\App\AppInterface;
 use Dealroadshow\K8S\Framework\Core\Container\ContainerInterface;
 use Dealroadshow\K8S\Framework\Core\Container\ContainerMaker;
 use Dealroadshow\K8S\Framework\Core\Container\ContainerMakerInterface;
-use Dealroadshow\K8S\Framework\Core\Container\EnvAwareContainerInterface;
+use Dealroadshow\K8S\Framework\Core\Container\Resources\ContainerResourcesInterface;
 use Dealroadshow\K8S\Framework\Core\Container\Resources\ResourcesConfigurator;
 use Dealroadshow\K8S\Framework\Util\Str;
 use ReflectionException;
@@ -21,46 +21,31 @@ class EnvAwareContainerMaker implements ContainerMakerInterface
     }
 
     /**
-     * @param ContainerInterface $builder
+     * @param ContainerInterface $manifest
      * @param VolumeList         $volumes
      * @param AppInterface       $app
      *
      * @return Container
      * @throws ReflectionException
      */
-    public function make(ContainerInterface $builder, VolumeList $volumes, AppInterface $app): Container
+    public function make(ContainerInterface $manifest, VolumeList $volumes, AppInterface $app): Container
     {
-        $container = $this->maker->make($builder, $volumes, $app);
+        $container = $this->maker->make($manifest, $volumes, $app);
         $resources = new ResourcesConfigurator($container->resources());
-        if ($builder instanceof EnvAwareContainerInterface) {
-            $builder->resourcesForEnv('default')?->apply($resources);
-        }
 
         $envSpecificResourcesMethod = 'resources'.Str::asClassName($this->env);
-        $class = new ReflectionObject($builder);
+        $class = new ReflectionObject($manifest);
         if (!$class->hasMethod($envSpecificResourcesMethod)) {
-            return $this->applyCallbacks($builder, $container);
+            return $container;
         }
 
         $method = $class->getMethod($envSpecificResourcesMethod);
         $params = $method->getParameters();
-        if (1 !== count($params) || !$params[0]->hasType() || ResourcesConfigurator::class !== $params[0]->getType()) {
-            return $this->applyCallbacks($builder, $container);
-        }
-
-        $method->invoke($builder, $resources);
-
-        return $this->applyCallbacks($builder, $container);
-    }
-
-    private function applyCallbacks(ContainerInterface $builder, Container $container): Container
-    {
-        if (!$builder instanceof EnvAwareContainerInterface) {
+        if (1 !== count($params) || !$params[0]->hasType() || ContainerResourcesInterface::class !== $params[0]->getType()) {
             return $container;
         }
 
-        $resources = new ResourcesConfigurator($container->resources());
-        $builder->resourcesForEnv($this->env)?->apply($resources);
+        $method->invoke($manifest, $resources);
 
         return $container;
     }
