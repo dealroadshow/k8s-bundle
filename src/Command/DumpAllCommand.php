1310d0e5bfd6827e347721c0a2ab64292c26cbb4
petr-buchyn <petr.buchyn@finsight.com>
2021-03-01 02:05:46 +0200

    Added "--recreate-output-dir" to DumpAllCommand

--- src/Command/DumpAllCommand.php
+++ src/Command/DumpAllCommand.php
@@ -7,11 +7,14 @@ use Dealroadshow\K8S\Framework\Dumper\AppDumper;
 use Dealroadshow\K8S\Framework\Registry\AppRegistry;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 class DumpAllCommand extends Command
 {
+    private const OPTION_RECREATE_DIR = 'recreate-output-dir';
+
     protected static $defaultName = 'dealroadshow_k8s:dump:all';
 
     public function __construct(
@@ -27,12 +30,23 @@ class DumpAllCommand extends Command
     {
         $this
             ->setDescription('Dumps all apps and their manifests to Yaml files')
+            ->addOption(
+                self::OPTION_RECREATE_DIR,
+                'R',
+                InputOption::VALUE_NONE,
+                'If specified, output directory will be deleted and recreated',
+                false
+            )
         ;
     }
 
     public function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
+        $recreateOutputDir = $input->getOption(self::OPTION_RECREATE_DIR);
+        if ($recreateOutputDir && file_exists($this->manifestsDir) && is_dir($this->manifestsDir)) {
+            rmdir($this->manifestsDir);
+        }
 
         foreach ($this->registry->aliases() as $alias) {
             $this->processor->process($alias);
