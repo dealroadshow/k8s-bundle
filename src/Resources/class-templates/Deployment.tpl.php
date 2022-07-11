358ec09d9fdf02d467f8a834228e59bdfe1b0e46
petr-buchyn <petr.buchyn@finsight.com>
2022-07-11 19:57:59 +0300

    Fixed Deployment class template

--- src/Resources/class-templates/Deployment.tpl.php
+++ src/Resources/class-templates/Deployment.tpl.php
@@ -4,6 +4,7 @@ namespace <?php echo $namespace; ?>;
 
 use Dealroadshow\K8S\Framework\Core\Container\Image\Image;
 use Dealroadshow\K8S\Framework\Core\Deployment\AbstractContainerDeployment;
+use Dealroadshow\K8S\Framework\Core\LabelSelector\SelectorConfigurator;
 
 class <?php echo $className; ?> extends AbstractContainerDeployment
 {
