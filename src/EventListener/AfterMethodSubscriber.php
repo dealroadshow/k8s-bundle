39f9e6e8eaccd7bf96fbb612520f71c7bcfe7106
petr-buchyn <petr.buchyn@finsight.com>
2023-05-16 00:59:15 +0300

    Fixed AfterMethodSubscriber

--- src/EventListener/AfterMethodSubscriber.php
+++ src/EventListener/AfterMethodSubscriber.php
@@ -6,7 +6,9 @@ namespace Dealroadshow\Bundle\K8SBundle\EventListener;
 
 use Dealroadshow\Bundle\K8SBundle\EnvManagement\Attribute\AfterMethod;
 use Dealroadshow\Bundle\K8SBundle\EventListener\Traits\ApplyWrappersTrait;
+use Dealroadshow\K8S\Framework\Event\ContainerMethodCalledEvent;
 use Dealroadshow\K8S\Framework\Event\ManifestMethodCalledEvent;
+use Dealroadshow\K8S\Framework\Event\ProxyableMethodCalledEventInterface;
 
 class AfterMethodSubscriber extends AbstractMethodResultSubscriber
 {
@@ -18,7 +20,7 @@ class AfterMethodSubscriber extends AbstractMethodResultSubscriber
     {
     }
 
-    protected function supports(ManifestMethodCalledEvent $event): bool
+    protected function supports(ProxyableMethodCalledEventInterface $event): bool
     {
         return true;
     }
@@ -26,12 +28,12 @@ class AfterMethodSubscriber extends AbstractMethodResultSubscriber
     /**
      * @throws \ReflectionException
      */
-    protected function afterMethod(ManifestMethodCalledEvent $event): void
+    protected function afterMethod(ProxyableMethodCalledEventInterface $event): void
     {
         $returnValue = self::NO_RETURN_VALUE;
 
         $this->applyWrappers(
-            proxyable: $event->manifest(),
+            proxyable: $event->proxyable(),
             methodName: $event->methodName(),
             params: $event->methodParams(),
             attributeClass: AfterMethod::class,
@@ -41,5 +43,13 @@ class AfterMethodSubscriber extends AbstractMethodResultSubscriber
         if (self::NO_RETURN_VALUE !== $returnValue) {
             $event->setReturnValue($returnValue);
         }
+    }
+
+    protected static function eventNames(): iterable
+    {
+        return [
+            ManifestMethodCalledEvent::NAME,
+            ContainerMethodCalledEvent::NAME,
+        ];
     }
 }
--- src/EventListener/ReplicasMethodSubscriber.php
+++ src/EventListener/ReplicasMethodSubscriber.php
@@ -4,17 +4,10 @@ declare(strict_types=1);
 
 namespace Dealroadshow\Bundle\K8SBundle\EventListener;
 
-use Dealroadshow\K8S\Framework\Event\ManifestMethodCalledEvent;
-
-class ReplicasMethodSubscriber extends AbstractEnvAwareMethodSubscriber
+class ReplicasMethodSubscriber extends AbstractEnvAwareManifestMethodSubscriber
 {
     public function methodName(): string
     {
         return 'replicas';
-    }
-
-    protected static function eventNames(): iterable
-    {
-        return [ManifestMethodCalledEvent::NAME];
     }
 }
