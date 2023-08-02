161ca84f9d664709fc74553154af535028937acd
petr-buchyn <petr.buchyn@finsight.com>
2023-08-03 02:41:29 +0300

    Fix in extension class

--- src/DependencyInjection/DealroadshowK8SExtension.php
+++ src/DependencyInjection/DealroadshowK8SExtension.php
@@ -74,6 +74,7 @@ class DealroadshowK8SExtension extends Extension
             ->setupNamespacePrefix($config['namespace_prefix'], $container)
         ;
 
+        $container->setParameter('dealroadshow_k8s.config.apps', $config['apps']);
         $container->setParameter('dealroadshow_k8s.auto_set_resources', $config['auto_set_resources']);
         $container->setParameter('dealroadshow_k8s.auto_set_replicas', $config['auto_set_replicas']);
     }
