665df7d1fc5787cd7e294719b0e7e687f7ecd56c
petr-buchyn <petr.buchyn@finsight.com>
2023-08-08 23:53:57 +0300

    Avoiding ambiguous behavior in AutoSet*Subscriber classes

--- src/EventListener/AutoSetReplicasSubscriber.php
+++ src/EventListener/AutoSetReplicasSubscriber.php
@@ -4,15 +4,22 @@ declare(strict_types=1);
 
 namespace Dealroadshow\Bundle\K8SBundle\EventListener;
 
+use Dealroadshow\Bundle\K8SBundle\EventListener\Traits\EnsureMethodIsNotDeclaredInUserManifestTrait;
 use Dealroadshow\Bundle\K8SBundle\Util\PropertyAccessUtil;
 use Dealroadshow\K8S\Framework\App\AppInterface;
 use Dealroadshow\K8S\Framework\Core\Deployment\DeploymentInterface;
 use Dealroadshow\K8S\Framework\Core\StatefulSet\StatefulSetInterface;
 use Dealroadshow\K8S\Framework\Event\ManifestMethodEvent;
 use Dealroadshow\K8S\Framework\Event\ProxyableMethodEventInterface;
+use Dealroadshow\Proximity\ProxyInterface;
 
 class AutoSetReplicasSubscriber extends AbstractMethodSubscriber
 {
+    use EnsureMethodIsNotDeclaredInUserManifestTrait;
+    public function __construct(private readonly string $env)
+    {
+    }
+
     protected function supports(ProxyableMethodEventInterface $event): bool
     {
         $manifest = $event->proxyable();
@@ -25,12 +32,19 @@ class AutoSetReplicasSubscriber extends AbstractMethodSubscriber
     {
         /** @var DeploymentInterface|StatefulSetInterface $manifest */
         $manifest = $event->proxyable();
+
         /** @var AppInterface $app */
         $app = PropertyAccessUtil::getPropertyValue($manifest, 'app');
         $replicas = $app->manifestConfig($manifest::shortName())['replicas'] ?? null;
         if ($replicas) {
+            $this->ensureMethodIsNotDeclaredInUserManifest($manifest, 'replicas', $app);
             $event->setReturnValue($replicas);
         }
+    }
+
+    private function checkIfReplicasMethodDeclared(): void
+    {
+
     }
 
     protected static function eventNames(): iterable
