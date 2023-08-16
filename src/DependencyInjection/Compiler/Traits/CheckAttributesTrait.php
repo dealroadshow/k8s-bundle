472832bdc912ff72ebdd841e717f397206c2f874
petr-buchyn <petr.buchyn@finsight.com>
2023-08-16 23:11:11 +0300

    Fix im CheckAttributesTrait

--- src/DependencyInjection/Compiler/Traits/CheckAttributesTrait.php
+++ src/DependencyInjection/Compiler/Traits/CheckAttributesTrait.php
@@ -47,7 +47,7 @@ trait CheckAttributesTrait
                 )
             );
         }
-        $parameter = $container->resolveEnvPlaceholders($container->getParameter($attribute->parameter));
+        $parameter = $container->resolveEnvPlaceholders($container->getParameter($attribute->parameter), true);
         if (!is_bool($parameter)) {
             throw new \RuntimeException(
                 sprintf(
