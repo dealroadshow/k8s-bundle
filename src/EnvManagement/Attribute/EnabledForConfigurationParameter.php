823688c857bdbccf9260bd40519bfac9175c633d
petr-buchyn <petr.buchyn@finsight.com>
2023-11-08 02:52:50 +0200

    Attribute EnabledForContainerParameter renamed to EnabledForConfigurationParameter

--- src/EnvManagement/Attribute/EnabledForConfigurationParameter.php
+++ src/EnvManagement/Attribute/EnabledForConfigurationParameter.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Dealroadshow\Bundle\K8SBundle\EnvManagement\Attribute;
 
 #[\Attribute(\Attribute::TARGET_CLASS)]
-readonly class EnabledForContainerParameter
+readonly class EnabledForConfigurationParameter
 {
     public function __construct(public string $parameter)
     {
