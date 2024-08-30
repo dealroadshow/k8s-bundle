5b8c341ce8870ca4108fcb63e71bebf484230283
petr-buchyn <54354491+petr-buchyn@users.noreply.github.com>
2024-08-31 00:46:43 +0300

    Util class renamed (#40)

---
+++ src/Util/ManifestNameUtil.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dealroadshow\Bundle\K8SBundle\Util;
+
+use Dealroadshow\K8S\Framework\Core\DynamicNameAwareInterface;
+use Dealroadshow\K8S\Framework\Core\ManifestInterface;
+
+final readonly class ManifestNameUtil
+{
+    public static function get(ManifestInterface $manifest): string
+    {
+        return $manifest instanceof DynamicNameAwareInterface
+            ? $manifest->name()
+            : $manifest::shortName();
+    }
+}
