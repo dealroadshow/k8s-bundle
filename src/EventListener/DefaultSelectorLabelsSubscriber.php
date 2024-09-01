c382e88e56e94e2148b88c4701b4e887bf900e98
petr-buchyn <54354491+petr-buchyn@users.noreply.github.com>
2024-09-01 03:21:32 +0300

    Default selector labels fix (#46)

--- src/EventListener/DefaultSelectorLabelsSubscriber.php
+++ src/EventListener/DefaultSelectorLabelsSubscriber.php
@@ -6,39 +6,40 @@ namespace Dealroadshow\Bundle\K8SBundle\EventListener;
 
 use Dealroadshow\Bundle\K8SBundle\Attribute\NoDefaultSelectorLabels;
 use Dealroadshow\Bundle\K8SBundle\Util\AttributesUtil;
-use Dealroadshow\K8S\Api\Apps\V1\Deployment;
 use Dealroadshow\K8S\Api\Apps\V1\StatefulSet;
+use Dealroadshow\K8S\Framework\Core\Deployment\DeploymentInterface;
+use Dealroadshow\K8S\Framework\Core\LabelSelector\SelectorConfigurator;
 use Dealroadshow\K8S\Framework\Core\LabelsGeneratorInterface;
-use Dealroadshow\K8S\Framework\Event\DeploymentGeneratedEvent;
-use Dealroadshow\K8S\Framework\Event\StatefulSetGeneratedEvent;
-use Dealroadshow\K8S\Framework\Util\ClassName;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Dealroadshow\K8S\Framework\Core\StatefulSet\StatefulSetInterface;
+use Dealroadshow\K8S\Framework\Event\ProxyableMethodEventInterface;
 
-readonly class DefaultSelectorLabelsSubscriber implements EventSubscriberInterface
+use Dealroadshow\K8S\Framework\Util\ClassName;
+
+class DefaultSelectorLabelsSubscriber extends AbstractManifestMethodSubscriber
 {
-    public function __construct(private LabelsGeneratorInterface $labelsGenerator)
+    public function __construct(private readonly LabelsGeneratorInterface $labelsGenerator)
     {
     }
 
-    protected function onManifestGenerated(DeploymentGeneratedEvent|StatefulSetGeneratedEvent $event): void
+    protected function supports(ProxyableMethodEventInterface $event): bool
     {
-        $manifest = $event->manifest();
+        $manifest = $event->proxyable();
+
+        return ($manifest instanceof DeploymentInterface || $manifest instanceof StatefulSet)
+            && 'selector' === $event->methodName();
+    }
+
+    protected function beforeMethod(ProxyableMethodEventInterface $event): void
+    {
+        /** @var DeploymentInterface|StatefulSetInterface $manifest */
+        $manifest = $event->proxyable();
         if ($this->classHasNoSelectorAttribute(ClassName::real($manifest))) {
             return;
         }
 
-        /** @var Deployment|StatefulSet $apiResource */
-        $apiResource = $event->apiResource();
-        $selector = $apiResource->spec()->selector()->matchLabels();
-        $selector->addAll($this->labelsGenerator->byManifestInstance($manifest));
-    }
-
-    public static function getSubscribedEvents(): array
-    {
-        return [
-            DeploymentGeneratedEvent::NAME => 'onManifestGenerated',
-            StatefulSetGeneratedEvent::NAME => 'onManifestGenerated',
-        ];
+        /** @var SelectorConfigurator $selector */
+        $selector = $event->methodParams()['selector'];
+        $selector->addLabels($this->labelsGenerator->byManifestInstance($manifest));
     }
 
     private function classHasNoSelectorAttribute(string $className): bool
