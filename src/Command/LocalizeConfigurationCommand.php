44c53ae4002b826a83f81934f1c75b5fd337cc51
petr-buchyn <54354491+petr-buchyn@users.noreply.github.com>
2024-10-13 18:08:13 +0300

    feat: `LocalizeConfigurationCommand` (#54)

---
+++ src/Command/LocalizeConfigurationCommand.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dealroadshow\Bundle\K8SBundle\Command;
+
+use Dealroadshow\K8S\Framework\App\Integration\ExternalEnvSourcesRegistry;
+use Dealroadshow\K8S\Framework\App\Integration\Localization\ExternalConfigurationLocalizer;
+use Dealroadshow\K8S\Framework\Registry\AppRegistry;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'dealroadshow_k8s:localize:configuration',
+    description: 'Localizes all external configurations (ConfigMaps, Secrets) for the specified apps',
+    aliases: ['k8s:localize:configuration', 'k8s:localize:config']
+)]
+class LocalizeConfigurationCommand extends Command
+{
+    public function __construct(
+        private readonly ExternalConfigurationLocalizer $localizer,
+        private readonly AppRegistry $appRegistry,
+        private readonly ExternalEnvSourcesRegistry $externalEnvSourcesRegistry,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument(
+                'apps',
+                InputArgument::IS_ARRAY | InputArgument::REQUIRED,
+                'Apps (aliases) to localize configurations for'
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $aliases = $input->getArgument('apps');
+        $apps = in_array('all', $aliases)
+            ? $this->appRegistry->all()
+            : array_map(fn (string $appAlias) => $this->appRegistry->get($appAlias), $aliases);
+
+        foreach ($apps as $app) {
+            $this->localizer->localizeDependencies(
+                $app->alias(),
+                $this->externalEnvSourcesRegistry->getForApp($app->alias())
+            );
+        }
+
+        return self::SUCCESS;
+    }
+}
