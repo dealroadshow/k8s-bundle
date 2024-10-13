ffefc865d8e8a000ee17188a83aca6c142ca4c7b
petr-buchyn <petr.buchyn@finsight.com>
2024-10-14 00:20:45 +0300

    fix: DummyLocalizationStrategy::localize() return type changed to void

--- src/App/Integration/DummyLocalizationStrategy.php
+++ src/App/Integration/DummyLocalizationStrategy.php
@@ -9,7 +9,7 @@ use Dealroadshow\K8S\Framework\App\Integration\Localization\LocalizationStrategy
 
 class DummyLocalizationStrategy extends AbstractLocalizationStrategy
 {
-    public function localize(string $dependentAppAlias, array $dependencies): mixed
+    public function localize(string $dependentAppAlias, array $dependencies): void
     {
         throw new \RuntimeException(sprintf('You must implement "%s" in your manifests application or to not use app configuration localization functionality.', LocalizationStrategyInterface::class));
     }
