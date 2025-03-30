c4350990d44a1255cfc901f6ecda3bf427ad86c0
petr-buchyn <petr.buchyn@finsight.com>
2025-03-30 19:22:45 +0300

    feat: replicas policies

---
+++ src/EventListener/ReplicasPoliciesSubscriber.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dealroadshow\Bundle\K8SBundle\EventListener;
+
+use Dealroadshow\Bundle\K8SBundle\EnvManagement\Container\ReplicasPolicyRegistry;
+use Dealroadshow\K8S\Framework\Core\Deployment\DeploymentInterface;
+use Dealroadshow\K8S\Framework\Event\ProxyableMethodCalledEventInterface;
+
+class ReplicasPoliciesSubscriber extends AbstractManifestMethodResultSubscriber
+{
+    public function __construct(
+        private readonly bool $deploymentReplicasPoliciesEnabled,
+        private readonly string $deploymentReplicasPoliciesEnv,
+        private readonly ReplicasPolicyRegistry $policies,
+    ) {
+    }
+
+    protected function supports(ProxyableMethodCalledEventInterface $event): bool
+    {
+        return $this->deploymentReplicasPoliciesEnabled &&
+            $event->proxyable() instanceof DeploymentInterface
+            && 'replicas' === $event->methodName();
+    }
+
+    protected function afterMethod(ProxyableMethodCalledEventInterface $event): void
+    {
+        $manifest = $event->proxyable();
+        $class = new \ReflectionClass($manifest);
+        $envAwareMethodName = 'replicas'.ucfirst($this->deploymentReplicasPoliciesEnv);
+        if ($class->hasMethod($envAwareMethodName)) {
+            $event->setReturnValue($class->getMethod($envAwareMethodName)->invoke($manifest));
+
+            return;
+        }
+
+        if ($this->policies->hasForEnv($this->deploymentReplicasPoliciesEnv) && 0 !== $event->returnedValue()) {
+            $event->setReturnValue($this->policies->getForEnv($this->deploymentReplicasPoliciesEnv));
+        }
+    }
+}
